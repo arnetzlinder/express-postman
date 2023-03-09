2a16e23f884bb0127dfdbbe6bc7cbc7d03b78195
Susanne Arnetz Linder <susanne.arnetzlinder@medieinstitutet.se>
2023-03-09 21:03:19 +0100

    add adding function for recepie

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,24 @@
 var express = require('express');
 var router = express.Router();
+const fs = require("fs");
+let recepies;
+
+
+fs.readFile("recepies.json", function(err, data){
+    if (err) {
+        console.log(err);
+    }
+    recepies = JSON.parse(data)
+    return;
+});
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
+
+router.get('/recepies', function (req, res, next) {
+  res.send(recepies)
+})
 
 module.exports = router;
