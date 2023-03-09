52fa72022f69cf332795346a9aba90161fc1dc6b
Susanne Arnetz Linder <susanne.arnetzlinder@medieinstitutet.se>
2023-03-09 21:03:29 +0100

    recepie added

--- app.js
+++ app.js
@@ -2,11 +2,14 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const cors = require('cors');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
+
+app.use(cors());
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -15,6 +18,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/recepies', usersRouter);
 
 module.exports = app;
--- routes/users.js
+++ routes/users.js
@@ -14,11 +14,24 @@ fs.readFile("recepies.json", function(err, data){
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.send(recepies);
 });
 
 router.get('/recepies', function (req, res, next) {
   res.send(recepies)
+})
+
+router.post('/add', function (req, res) {
+  res.send({message: "Tack för ditt fantastiska tillägg"});
+let newRecepieName = req.body.recepieName;
+let newDescription = req.body.description;
+let newIngredient = req.body.ingredients;
+let newPreptime = req.body.prepTime;
+let newRecepieId = recepies.length + 1;
+let NewRecepie = NewRecepie(newRecepieName, newDescription, newIngredient, newPreptime, newRecepieId);
+recepies.push(newRecepie);
+console.log (recepies);
+recepies.json(recepies);
 })
 
 module.exports = router;
